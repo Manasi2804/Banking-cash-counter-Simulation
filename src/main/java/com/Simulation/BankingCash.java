a01decfa326490ff1681c70206727de460334c4e
Manasi2804 <shahmanasi281996@gmail.com>
2020-04-05 09:53:34 +0530

    Added: Simulation Using queue

--- src/main/java/com/Simulation/BankingCash.java
+++ src/main/java/com/Simulation/BankingCash.java
@@ -1,7 +1,70 @@
 package com.Simulation;
+import java.util.Scanner;
 
-public class BankingCash {
-    public static void main(String[] args) {
-        System.out.println("Welcome to bank cash counter");
+public  class BankingCash {
+
+    public static class Queue {
+        static int amount = 10000;
+        //method deposit amount add to original amount
+        public  int insert(int value)
+        {
+            amount = amount + value;
+            return amount;
+        }
+
+        //method withdraw amount from original amount
+        public int remove(int value)
+        {
+            amount=amount-value;
+            return amount;
+        }
+        //method check bank balance maintance or not
+        public  boolean checkBankBalance(int value1,int amount1)
+        {
+            if(value1==amount1)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+    //main method
+    public static void main(String args[])
+    {
+        int result1=0;
+        System.out.println("welcome to Banking Cash Counter");
+
+        //class object created
+        Queue object=new Queue();
+        Scanner sc=new Scanner(System.in);
+        System.out.println("how many people in queue");
+        int peopleCount=sc.nextInt();
+
+        while(peopleCount>0)
+        {
+            peopleCount--;
+            System.out.println("enter value");
+            int val = sc.nextInt();
+            System.out.println("ask for choice");
+            int choice = sc.nextInt();
+            switch (choice)
+            {
+                case 1:
+                    System.out.println("Deposit money");
+                    result1 = (object.insert(val));
+                    System.out.println(result1);
+                    break;
+                case 2:
+                    System.out.println("Withdraw money");
+                    result1 = (object.remove(val));
+                    System.out.println(result1);
+                    break;
+                default:
+                    System.out.println("invalid choice");
+            }
+        }
+        System.out.println(object.checkBankBalance(result1,Queue.amount));
     }
 }
+
+
